4cc89a920933ae95426d45d2805d6c5255435335
Robert Kubasiak <robert2006goetel@gmail.com>
2023-12-22 20:07:20 +0100

    Changing players' name

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,35 @@
-export default function Player({name, symbol}) {
+import {useState} from "react";
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleChange(event) {
+        setPlayerName(event.target.value)
+    }
+
+    function handleEditClick() {
+        // setIsEditing(!isEditing) // => schedules a state update to true
+
+        // Change the latest value of state (isEditing is auto pass to editing)
+        setIsEditing((editing) => !editing)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    let btnCaption = "Edit"
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>  // Default value
+        btnCaption = "Save"
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{btnCaption}</button>
         </li>
-    )
+    );
 };
